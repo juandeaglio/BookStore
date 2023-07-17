a66a0a005eb764d3b0bb8016bc9cec7aba5a4862
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-07-17 14:43:36 -0400

    Refactored log in view.

--- catalog_service/views.py
+++ catalog_service/views.py
@@ -17,22 +17,38 @@ def fetchCatalog(request):
 
 
 def loginUser(request):
-    if os.environ.get('ENVIRONMENT') == "test" and os.environ.get('TESTPW') and os.environ.get('TESTUSERNAME'):
-        if authenticate(request, username=os.environ.get('TESTUSERNAME'), password=os.environ.get('TESTPW')) is None:
-            User.objects.create_user(username=os.environ.get('TESTUSERNAME'), password=os.environ.get('TESTPW'))
+    createTestCatalogAdminUser(request)
 
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request, username=username, password=password)
+        return attemptLogin(password, request, username)
 
-        if user is not None:
-            login(request, user)
-            return HttpResponse(request)
-        else:
-            return HttpResponse('Unauthorized', status=401)
     return HttpResponse('Not found', status=404)
+
+
+def attemptLogin(password, request, username):
+    try:
+        loginWithCredentials(request, username, password)
+        return HttpResponse(request)
+
+    except ValueError as loginFailed:
+        return HttpResponse('Unauthorized', status=401)
+
+
+def loginWithCredentials(request, username, password):
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        raise(ValueError("Unauthorized"))
+
+
+def createTestCatalogAdminUser(request):
+    if os.environ.get('ENVIRONMENT') == "test" and os.environ.get('TESTPW') and os.environ.get('TESTUSERNAME'):
+        if authenticate(request, username=os.environ.get('TESTUSERNAME'), password=os.environ.get('TESTPW')) is None:
+            User.objects.create_user(username=os.environ.get('TESTUSERNAME'), password=os.environ.get('TESTPW'))
 
 
 def addBook(request):
