6d72d4bf89591e927c299082d868d5f158abb862
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-24 17:00:11 -0400

    Extra debug

--- Source/WebServerStrategy/GunicornNginxStrategy.py
+++ Source/WebServerStrategy/GunicornNginxStrategy.py
@@ -10,7 +10,7 @@ class GunicornNginxStrategy(WebServerStrategy):
     def start(self):
         self.gunicornProcess = self.gunicorn.start()
         if self.osLibrary.name == 'posix':
-            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout)
+            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout.decode('utf-8'))
             return self.subprocessLib.Popen(['sudo','nginx', '-g daemon off;'])
 
     def createStopCommand(self):
@@ -26,6 +26,6 @@ class GunicornNginxStrategy(WebServerStrategy):
         if self.osLibrary.name == 'posix':
             cmd2 = "CheckRunGunicorn.sh"
             cmd3 = "CheckRunNginx.sh"
-            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout)
+            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout.decode('utf-8')))
             return self.subprocessLib.run(["bash", cmd2], capture_output=True).returncode > 0 and \
                 self.subprocessLib.run(["bash", cmd3], capture_output=True).returncode > 0
