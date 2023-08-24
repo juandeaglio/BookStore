59a7c78fb99c6220b8308034fad3d93cf181c65b
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-24 15:33:06 -0400

    Adding sudo to nginx command

--- Source/WebServerStrategy/GunicornNginxStrategy.py
+++ Source/WebServerStrategy/GunicornNginxStrategy.py
@@ -10,7 +10,10 @@ class GunicornNginxStrategy(WebServerStrategy):
     def start(self):
         self.gunicornProcess = self.gunicorn.start()
         if self.osLibrary.name == 'posix':
-            return self.subprocessLib.Popen(['nginx', '-g daemon off;'])
+            if self.subprocessLib.Popen(["sudo", "ls", "-lhrt"]).resultcode == 0:
+                return self.subprocessLib.Popen(['sudo','nginx', '-g daemon off;'])
+            else:
+                return self.subprocessLib.Popen(['nginx', '-g daemon off;'])
 
     def createStopCommand(self):
         cmd = ''
