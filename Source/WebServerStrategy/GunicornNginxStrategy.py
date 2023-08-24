b3faf45eab0374c4146b4a52aa823c060335adb6
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-24 17:04:43 -0400

    Parentheses error..

--- Source/WebServerStrategy/GunicornNginxStrategy.py
+++ Source/WebServerStrategy/GunicornNginxStrategy.py
@@ -26,6 +26,6 @@ class GunicornNginxStrategy(WebServerStrategy):
         if self.osLibrary.name == 'posix':
             cmd2 = "CheckRunGunicorn.sh"
             cmd3 = "CheckRunNginx.sh"
-            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout.decode('utf-8')))
+            print(self.subprocessLib.run(["bash", "ps -efw"], capture_output=True).stdout.decode('utf-8'))
             return self.subprocessLib.run(["bash", cmd2], capture_output=True).returncode > 0 and \
                 self.subprocessLib.run(["bash", cmd3], capture_output=True).returncode > 0
