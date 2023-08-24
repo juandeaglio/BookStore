88ee7270f4898367e00a9d44ccc2961ab1b07e7b
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-24 12:14:02 -0400

    Attempt two at fixing CheckRunGunicorn.sh

--- Source/WebServerStrategy/GunicornStrategy.py
+++ Source/WebServerStrategy/GunicornStrategy.py
@@ -20,7 +20,5 @@ class GunicornStrategy(WebServerStrategy):
 
     def isRunning(self):
         if self.osLibrary.name == 'posix':
-            cmd = "CheckRunDjango.sh"
             cmd2 = "CheckRunGunicorn.sh"
-            return self.subprocessLib.run(["bash", cmd], capture_output=True).returncode > 0 \
-                and self.subprocessLib.run(["bash", cmd2], capture_output=True).returncode > 0
+            return self.subprocessLib.run(["bash", cmd2], capture_output=True).returncode > 0
