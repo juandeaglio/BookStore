18364dc02e9070f6d80ee54fac9fc360fe893f64
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-29 15:02:51 -0400

    Added root to nginx, maybe it will pass bdd testing.

--- nginxTemplate.py
+++ nginxTemplate.py
@@ -1,12 +1,13 @@
 config="""server{
-    user root;
     listen {nginx_port};
     server_name {server_name};
 
     location /static/ {
+        user root;
         alias {static_path}/;
     }
     location /static/imgs/ {
+        user root;
         alias {static_path}/imgs/;
     }
 
