42789f2a4d2cc711c953c84accc8214970578793
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-29 16:46:47 -0400

    Modified to print config file for debugging.

--- Acceptance/BookStore/Steps/Spec_WebServer.py
+++ Acceptance/BookStore/Steps/Spec_WebServer.py
@@ -98,6 +98,7 @@ def seeStaticImage(context):
     if os.name == 'nt' and (context.web_server_type == "GunicornNginx"):
         assert os.name == 'nt'
     else:
+        print(context.web_server.inspectedConfigFile)
         assert context.response.status_code == 200, "Expected 200 OK but got " + str(context.response.status_code)
         assert context.response.headers['Content-Type'] == 'image/jpeg', "Expected image/png but got " + \
                                                                         str(context.response.headers['Content-Type'])
